c15f0b774f2aff50e48db4cc31dc595c3ad73098
Fareedah AbdulRasaq <Fareedah@fareedah-MacBook-Air.local>
2024-08-27 16:41:59 +0100

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,25 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// import { Html, Head, Main, NextScript } from "next/document";
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+import { Head, Html, Main, NextScript } from "next/document";
+import { ColorSchemeScript } from "@mantine/core";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </Head>
       <body>
         <Main />
         <NextScript />
